9345a369fffa85afaf59ebef4cf68f27f1fb173b
budbudmike <40642152+budbudmike@users.noreply.github.com>
2020-12-08 20:32:42 +0100

    Final Push

---
+++ src/main/java/semantic/controler/DoItYourselfControl.java
@@ -0,0 +1,38 @@
+package semantic.controler;
+
+import java.util.Iterator;
+import java.util.List;
+
+import semantic.model.IConvenienceInterface;
+import semantic.model.IModelFunctions;
+import semantic.model.ObservationEntity;
+
+public class DoItYourselfControl implements IControlFunctions
+{
+	private IConvenienceInterface model;
+	private IModelFunctions customModel;
+	
+	public DoItYourselfControl(IConvenienceInterface model, IModelFunctions customModel)
+	{
+		this.model = model;
+		this.customModel = customModel;
+	}
+	
+	@Override
+	public void instantiateObservations(List<ObservationEntity> obsList,
+			String paramURI) {
+		int total=obsList.size();
+		int count=0;
+		for(Iterator<ObservationEntity> it=obsList.iterator(); it.hasNext();)
+		{	count ++; 
+			ObservationEntity aux=it.next();
+			String bloup =customModel.createObs(aux.getValue().toString(), paramURI, customModel.createInstant(aux.getTimestamp()));
+		System.out.print("Element ");
+		System.out.print(count);
+		System.out.print(" of ");
+		System.out.println(total);
+
+		}
+		
+	}
+}
